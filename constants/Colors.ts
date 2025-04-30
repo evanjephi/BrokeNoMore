ad8fe8f531ec9e4ae5b1c495512d6d985d3adaa4
Even <evanjephi@gmail.com>
2025-04-29 22:07:09 -0400

    added error and susccess colors

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -14,6 +14,8 @@ export const Colors = {
     icon: '#687076',
     tabIconDefault: '#687076',
     tabIconSelected: tintColorLight,
+    error: '#ff4d4d',
+    success: '#4CAF50',
   },
   dark: {
     text: '#ECEDEE',
@@ -22,5 +24,7 @@ export const Colors = {
     icon: '#9BA1A6',
     tabIconDefault: '#9BA1A6',
     tabIconSelected: tintColorDark,
+    error: '#ff4d4d',
+    success: '#4CAF50',
   },
 };
