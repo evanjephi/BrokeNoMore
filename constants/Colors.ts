7fd7ef84c2d55df9e1a64d2e0dd720acd3c60e8a
Even <evanjephi@gmail.com>
2025-05-03 17:05:22 -0400

    colors updated

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,25 +3,32 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
+const tintColorLight = '#003366';
+const tintColorDark = '#fff';
+
 export const Colors = {
   light: {
-    text: '#212121', // Primary Text
-    secondaryText: '#4F4F4F', // Secondary Text
-    background: '#FFFFFF', // Background
-    border: '#E0E0E0', // Borders/Dividers
-    primary: '#003366',
-    secondary: '#4CAF50',
-    error: '#ff4d4d',
-    success: '#4CAF50',
+    text: '#11181C', // Darker text for better contrast when using light mode
+    background: '#fff', // White background for light mode
+    backgroundDark: '#F0F0F0', // Slightly darker background for better contrast
+    backgroundLight: '#e3e1e1',
+    tint: tintColorLight,
+    icon: '#687076',
+    tabIconDefault: '#687076',
+    tabIconSelected: tintColorLight,
+    error: '#FF3B30',
+    success: '#4CD964',
   },
   dark: {
-    text: '#E0E0E0', // Primary Text
-    secondaryText: '#B0B0B0', // Secondary Text
-    background: '#121212', // Background
-    border: '#2C2C2C', // Borders/Dividers
-    primary: '#FFFFFF',
-    secondary: '#4CAF50',
-    error: '#ff4d4d',
-    success: '#4CAF50',
+    text: '#ECEDEE', // Lighter text for better contrast when using dark mode
+    background: '#121212', // Dark background for dark mode
+    backgroundLight: '#1F1F1F', // Slightly lighter background for better contrast
+    backgroundDark: '#1F1F1F', // Darker background for better contrast
+    tint: tintColorDark,  
+    icon: '#9BA1A6', // Lighter icon color for better contrast
+    tabIconDefault: '#9BA1A6',
+    tabIconSelected: tintColorDark,
+    error: '#FF3B30',
+    success: '#4CD964',
   },
 };
