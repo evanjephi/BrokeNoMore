57765ffa76d58143b41f044ae81879749d83a72a
Even <evanjephi@gmail.com>
2025-05-25 23:06:59 -0400

    created global variable color for bgInput

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -7,11 +7,13 @@ const tintColorLight = '#003366';
 const tintColorDark = '#fff';
 const yellowTitleColorLight = '#003366'; //yelowish color for light mode
 const greenTitleColorDark = '#77d19e'; // greenish color for dark mode
+const bgDark = '#121212'; // Dark background color for dark mode
+const bgLight = '#fff'; // Light background color for light mode
 
 export const Colors = {
   light: {
     text: '#11181C', // Darker text for better contrast when using light mode
-    background: '#fff', // White background for light mode
+    background: bgLight, // White background for light mode
     subTitleColor: yellowTitleColorLight, // Light gray background for secondary elements
     tint: tintColorLight,
     icon: '#687076',
@@ -19,10 +21,11 @@ export const Colors = {
     tabIconSelected: tintColorLight,
     error: '#FF3B30',
     success: '#4CD964',
+    inputBackground: bgLight,
   },
   dark: {
     text: '#ECEDEE', // Lighter text for better contrast when using dark mode
-    background: '#121212', // Dark background for dark mode
+    background: bgDark, // Dark background for dark mode
     subTitleColor: greenTitleColorDark, // Light gray background for secondary elements
     tint: tintColorDark,  
     icon: '#9BA1A6', // Lighter icon color for better contrast
@@ -30,5 +33,6 @@ export const Colors = {
     tabIconSelected: tintColorDark,
     error: '#FF3B30',
     success: '#4CD964',
+    inputBackground: bgDark,
   },
 };
