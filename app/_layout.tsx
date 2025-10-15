bf77c813ea9e6560fdcf654f227fb9746d1d3959
Even Yohans <73504127+evanjephi@users.noreply.github.com>
2025-10-14 22:52:43 -0400

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,13 +3,12 @@ import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { useEffect } from 'react'
+import 'react-native-reanimated'
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
-// Prevent the splash screen from auto-hiding 
-// before asset loading is complete.
+// Prevent the splash screen from auto-hiding before asset loading is complete
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
